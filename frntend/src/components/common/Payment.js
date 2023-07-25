633e24a1ad1dff9471f71a449c1f33a8f31335e1
hardikralhan66@gmail.com <hardikralhan66@gmail.com>
2023-07-25 13:20:42 +0530

    payment page

--- frntend/src/components/common/Payment.js
+++ frntend/src/components/common/Payment.js
@@ -73,7 +73,7 @@ export function Payment() {
                             // value={currency}
                             // onChange={(e) => {
                             //     setCurrency(e.target.value);
-                            //     handleCurrencyChange(e.target.value);
+                            //     handleCurrencyChange(e.target.value); 
                             // }}
                             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-lg text-md px-5 py-3 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 my-3"
                         >
